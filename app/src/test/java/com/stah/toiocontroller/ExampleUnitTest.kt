cd9c45d15e9b30988bc9fd661e414a9565086bb5
yoshida0261 <yoshida261@gmail.com>
2019-06-20 15:54:51 +0900

    refactor: ktlin format

--- app/src/test/java/com/stah/toiocontroller/ExampleUnitTest.kt
+++ app/src/test/java/com/stah/toiocontroller/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.stah.toiocontroller
 
 import org.junit.Test
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
