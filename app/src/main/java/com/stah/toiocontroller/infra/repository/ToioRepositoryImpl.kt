ea9b8c62c0e2de5837c7ce38a045b4df4ae0cb4e
yoshida0261 <yoshida261@gmail.com>
2019-07-02 11:24:00 +0900

    refactor: 不要な変数を削除

--- app/src/main/java/com/stah/toiocontroller/infra/repository/ToioRepositoryImpl.kt
+++ app/src/main/java/com/stah/toiocontroller/infra/repository/ToioRepositoryImpl.kt
@@ -2,7 +2,6 @@ package com.stah.toiocontroller.infra.repository
 
 import android.os.ParcelUuid
 import com.polidea.rxandroidble2.RxBleClient
-import com.polidea.rxandroidble2.RxBleDevice
 import com.polidea.rxandroidble2.scan.ScanSettings
 import com.stah.toiocontroller.domain.ToioCube
 import com.stah.toiocontroller.domain.repository.ToioRepository
@@ -13,8 +12,7 @@ import timber.log.Timber
 class ToioRepositoryImpl(val bleClient: RxBleClient) : ToioRepository {
 
     lateinit var dispose: Disposable
-    val mSessionManager = BleSessionManager(bleClient, SchedulerProvider)
-    lateinit var bleDevice: RxBleDevice
+    private val sessionManager = BleSessionManager(bleClient, SchedulerProvider)
 
     override fun scan() {
         dispose = bleClient.scanBleDevices(
@@ -24,29 +22,23 @@ class ToioRepositoryImpl(val bleClient: RxBleClient) : ToioRepository {
             Timber.d("toio uuid ${it.scanRecord.serviceUuids}")
             it.scanRecord.serviceUuids!!.contains(ParcelUuid(ToioCube.SERVICE_UUID))
         }
-            // .timeout(15, TimeUnit.SECONDS)
             .subscribe({
                 Timber.d("toio name ${it.bleDevice.name}")
-
                 Timber.d("toio: mac ${it.bleDevice.macAddress}")
 
-                mSessionManager.setMacAddress(it.bleDevice.macAddress)
-                mSessionManager.setupConnection()
-                bleDevice = it.bleDevice
-                // connect = it.bleDevice.establishConnection(false)
-                // connect()
+                sessionManager.setMacAddress(it.bleDevice.macAddress)
+                sessionManager.setupConnection()
 
                 dispose.dispose()
             }, {
                 Timber.e(it.localizedMessage)
             })
-        // dispose.dispose()
     }
 
 
     override fun front() {
 
-        dispose = mSessionManager.connection.flatMapSingle {
+        dispose = sessionManager.connection.flatMapSingle {
             it.writeCharacteristic(ToioCube.MOTOR_UUID, byteArrayOf(0x01, 0x01, 0x01, 0x64, 0x02, 0x02, 0x14))
         }.subscribe({
             println("toio cube front ")
@@ -73,7 +65,7 @@ class ToioRepositoryImpl(val bleClient: RxBleClient) : ToioRepository {
 
     override fun disconnect() {
         Timber.d("toio disconnect")
-        mSessionManager.disconnect()
+        sessionManager.disconnect()
     }
 }
 
