a7ddd7999b6b273f42038e3579f6957c904f74cd
yoshida0261 <yoshida261@gmail.com>
2020-08-08 05:29:46 +0900

    接続用の画面を追加

---
+++ app/src/main/java/com/stah/toiocontroller/activity/ConnectActivity.kt
@@ -0,0 +1,16 @@
+package com.stah.toiocontroller.activity
+
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import androidx.appcompat.app.AppCompatActivity
+import com.stah.toiocontroller.R
+
+class ConnectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connect)
+
+
+    }
+}
