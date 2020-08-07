b7ba8fa3b6268a78bf26910277fce967729b4fd4
yoshida0261 <yoshida261@gmail.com>
2020-08-08 05:40:53 +0900

    watchを削除

--- app/src/main/java/com/stah/toiocontroller/activity/ConnectActivity.kt
+++ app/src/main/java/com/stah/toiocontroller/activity/ConnectActivity.kt
@@ -1,7 +1,6 @@
 package com.stah.toiocontroller.activity
 
 import android.os.Bundle
-import android.support.wearable.activity.WearableActivity
 import androidx.appcompat.app.AppCompatActivity
 import com.stah.toiocontroller.R
 
