12549c6d40b5aed6258ab11195e2f1db3e32bf01
yoshida0261 <yoshida261@gmail.com>
2020-08-09 14:40:07 +0900

    OnPermissionDenied追加

--- app/src/main/java/com/stah/toiocontroller/activity/ConnectActivity.kt
+++ app/src/main/java/com/stah/toiocontroller/activity/ConnectActivity.kt
@@ -2,10 +2,12 @@ package com.stah.toiocontroller.activity
 
 import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.stah.toiocontroller.R
 import kotlinx.android.synthetic.main.activity_connect.*
 import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnPermissionDenied
 import permissions.dispatcher.RuntimePermissions
 
 @RuntimePermissions
@@ -14,6 +16,11 @@ class ConnectActivity : AppCompatActivity() {
     @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
     fun scanToioCube() {
 
+    }
+
+    @OnPermissionDenied(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun onLocationDeneed() {
+        Toast.makeText(this, "位置情報を有効にしないとこのアプリは使用できません", Toast.LENGTH_LONG).show()
     }
 
 
